44ef46187ae6f45ebbc5b6b6de345bf73a5460f9
Hennequin <pythoncraft2@gmail.com>
2021-04-13 22:28:04 +0200

    [UPDATE] index.js server opti

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
 const path = require('path');
-const jsdom = require('jsdom');
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const datauri = require('datauri');
-const { JSDOM } = jsdom;
+const { JSDOM } = require('jsdom');
 
 app.use(express.static(__dirname + '/public'));
 
