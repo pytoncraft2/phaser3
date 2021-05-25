1f2b135a9c756006b90f86a9cd000ed876f1697e
Hennequin <pythoncraft2@gmail.com>
2021-05-25 19:23:02 +0200

    [📍] save

--- server/authoritative_server/js/game.js
+++ server/authoritative_server/js/game.js
@@ -140,16 +140,25 @@ function update() {
       player.anim = 'heal';
     }
 
+
+      if (player.y < 100) {
+      console.log('inf 100');
+      player.setVelocityY(-100)
+    }
+
     if (input.space) {
       const base = player.y;
       console.log(base);
-      if (player.y < 100)
+      /*
+      if (player.y < 100) {
       console.log('inf 100');
       player.setVelocityY(100)
+    }
+    */
     } else {
-      if (player.y > 400) {
-      player.setVelocityY(-100)
-      }
+      // if (player.y > 400) {
+      // player.setVelocityY(-100)
+      // }
     }
 /*
     if (input.left) {
