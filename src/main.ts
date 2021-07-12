f4357f397ccd57ed55607da8e78876c2931add02
Hennequin <pythoncraft2@gmail.com>
2021-07-12 22:54:32 +0200

    [!] pas d'affichage d'image

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,25 @@ import Phaser from 'phaser'
 // import HelloWorldScene from './scenes/HelloWorldScene'
 import Solo from './scenes/Solo'
 import Menu from './scenes/Menu'
+import Multijoueur from './scenes/Multijoueur'
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 }
-		}
+  scale: {
+  mode: Phaser.Scale.CENTER_BOTH,
+  autoCenter: Phaser.Scale.CENTER_BOTH
+},
+  backgroundColor: 'black',
+  width: 1500,
+  height: 720,
+  physics: {
+    default: 'arcade',
+    arcade: {
+        gravity: { y: 0 },
+        debug: false
+    }
 	},
-	scene: [Menu, Solo]
+	scene: [Menu, Solo, Multijoueur]
 }
 
 export default new Phaser.Game(config)
